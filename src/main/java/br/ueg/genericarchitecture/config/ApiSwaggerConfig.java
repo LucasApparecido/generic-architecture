baef63fdec5032cba78ceeffe20fefeea093ab08
Lucas Apparecido de Oliveira <108204218+LucasApparecido@users.noreply.github.com>
2025-04-30 14:09:56 -0300

    fix: correção de nomenclatura para geração das rotas no front

--- src/main/java/br/ueg/genericarchitecture/config/ApiSwaggerConfig.java
+++ src/main/java/br/ueg/genericarchitecture/config/ApiSwaggerConfig.java
@@ -16,8 +16,8 @@ public class ApiSwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI().addSecurityItem(new SecurityRequirement().
-                        addList("Bearer Authentication"))
+                        addList("Bearer_Authentication"))
                 .components(new Components().addSecuritySchemes
-                        ("Bearer Authentication", createAPIKeyScheme()));
+                        ("Bearer_Authentication", createAPIKeyScheme()));
     }
 }
